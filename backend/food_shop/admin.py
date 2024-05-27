6cf10522070ac70aa69eac86f64af028be98f85a
Dmitry Pavlenko <125051658+DPavlen@users.noreply.github.com>
2024-05-27 14:21:59 +0300

    App food_shop, add models-admin Product, ProductCart, ShoppingCartProduct

--- backend/food_shop/admin.py
+++ backend/food_shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from food_shop.models import Category, Subcategory
+from food_shop.models import Category, Subcategory, Product, ProductCart, ShoppingCartProduct
 
 
 @admin.register(Category)
@@ -17,4 +17,41 @@ class SubcategoryAdmin(admin.ModelAdmin):
     list_display = ("pk", "name", "slug", "category",)
     search_fields = ("name", "slug")
     list_filter = ("category",)
+    empty_value_display = "-пусто-"
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Настроенная панель админки продуктов."""
+    list_display = ("pk", "name", "date_add", "slug", "category", "price")
+    search_fields = ("name", "slug", "date_add", "price")
+    list_filter = ("name","category", "price")
+    empty_value_display = "-пусто-"
+
+    def formatted_date_add(self, obj):
+        return obj.date_add.strftime("%B %d, %Y, %I:%M %p")
+
+    formatted_date_add.short_description = "Дата добавления продукта"
+
+
+@admin.register(ProductCart)
+class ProductCartAdmin(admin.ModelAdmin):
+    """Настроенная панель админки продуктовой корзины."""
+    list_display = ("pk", "buyer", "date_create",)
+    search_fields = ("buyer", "buyer", "date_create",)
+    list_filter = ("buyer", "date_create",)
+    empty_value_display = "-пусто-"
+
+    def formatted_date_create(self, obj):
+        return obj.date_create.strftime("%B %d, %Y, %I:%M %p")
+
+    formatted_date_create.short_description = "Дата создания корзины"
+
+
+@admin.register(ShoppingCartProduct)
+class ShoppingCartProductAdmin(admin.ModelAdmin):
+    """Настроенная панель админки продуктовой корзины товаров."""
+    list_display = ("pk", "product_cart", "product", "amount")
+    search_fields = ("pk", "product_cart", "product", "amount")
+    list_filter = ("product_cart", "product",)
     empty_value_display = "-пусто-"
--- backend/food_shop/models.py
+++ backend/food_shop/models.py
@@ -27,15 +27,9 @@ class Category(models.Model):
         unique=True,
         verbose_name="Слаг категории"
     )
-    description = models.CharField(
-        max_length=250,
-        default=None,
-        blank=True,
-        verbose_name="Описание категории"
-    )
     icon = models.ImageField(
         verbose_name="Фото категории",
-        upload_to="food_shop/images/categories",
+        upload_to="categories",
         default=None,
         blank=True,
     )
@@ -71,8 +65,8 @@ class Subcategory(models.Model):
         verbose_name="Cлаг подкатегории"
     )
     icon = models.ImageField(
-        "Изображение",
-        upload_to="food_shop/images/subcategories",
+        "Фото подкатегории",
+        upload_to="subcategories",
         default=None,
         blank=True,
     )
@@ -88,3 +82,121 @@ class Subcategory(models.Model):
         """
 
         return self.name
+
+
+class Product(models.Model):
+    """Модель продукта."""
+    name = models.CharField(
+        unique=True,
+        max_length=250,
+        verbose_name="Название продукта"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="Категория"
+    )
+    subcategory = models.ForeignKey(
+        Subcategory,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="Подкатегория"
+    )
+    slug = AutoSlugField(
+        unique=True,
+        max_length=200,
+        populate_from=get_slug,
+        verbose_name="Cлаг продукта"
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Стоимость продукта"
+    )
+    icon_small = models.ImageField(
+        upload_to="products_small/",
+        blank=True,
+        null=True,
+        verbose_name="Фото продукта маленькое",
+    )
+    icon_middle = models.ImageField(
+        upload_to="products_middle/",
+        blank=True,
+        null=True,
+        verbose_name="Фото продукта среднее",
+    )
+    icon_big = models.ImageField(
+        upload_to="products_big/",
+        blank=True,
+        null=True,
+        verbose_name="Фото продукта большое",
+    )
+    date_add = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата добавления продукта"
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ("date_add",)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class ProductCart(models.Model):
+    """Модель продуктовой корзины у покупателя-пользователя."""
+    buyer = models.ForeignKey(
+        MyUser,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь продуктовой корзины"
+    )
+    date_create = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания продуктовой корзины"
+    )
+
+    class Meta:
+        verbose_name = "Продуктовая корзина"
+        verbose_name_plural = "Продуктовые корзины"
+        ordering = ("date_create",)
+
+    def __str__(self):
+        return f"Покупатель {self.buyer}"
+
+
+class ShoppingCartProduct(models.Model):
+    """Модель корзины покупок товаров.
+    Связка между продуктовой корзины покупателя-пользователя
+    и продуктами.
+    """
+    product_cart = models.ForeignKey(
+        ProductCart,
+        on_delete=models.CASCADE,
+        related_name="shopping_cart_products",
+        verbose_name="Продуктовая корзина"
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name="Продукт"
+    )
+    amount = models.PositiveSmallIntegerField(
+        default=1,
+        verbose_name="Количество"
+    )
+
+    class Meta:
+        unique_together = ("product_cart", "product")
+        verbose_name = "Продукт в корзине у пользователя"
+        verbose_name_plural = "Продукты в корзинах у пользователей"
+
+    def __str__(self):
+        return f"В продуктовой козине {self.product.name} - {self.amount} шт."
+
+
+
+
